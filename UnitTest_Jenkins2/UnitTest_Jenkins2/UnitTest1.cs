9dadf2ccb069d485e9475ec03e5bdc5c0f8217a7
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2019-10-08 15:44:52 -0500

    commit project

---
+++ UnitTest_Jenkins2/UnitTest_Jenkins2/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest_Jenkins2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("New Test for Jenkins");
+        }
+    }
+}
